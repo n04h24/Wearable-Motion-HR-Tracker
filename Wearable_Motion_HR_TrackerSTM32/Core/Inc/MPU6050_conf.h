9bdcd5b485b6e92c13fc371161b59badc8b87af1
n04h24 <noahnourdine8@gmail.com>
2025-11-21 08:12:21 +1100

    Struct Error (Offset Retrieval)

--- Wearable_Motion_HR_TrackerSTM32/Core/Inc/MPU6050_conf.h
+++ Wearable_Motion_HR_TrackerSTM32/Core/Inc/MPU6050_conf.h
@@ -16,6 +16,7 @@
 
 /* Externs */
 extern uint8_t check_memory;
+//Universal register value buffer
 
 typedef struct  {
 
@@ -24,8 +25,29 @@ typedef struct  {
 	float Za;
 
 } FactoryTrims;
-
+//Store internal characteristics
 extern FactoryTrims FT;
+//Initialise as 'FT'
+
+
+typedef struct {
+
+	float x;
+	float y;
+	float z;
+
+} Offsets;
+//Store offsets from calibration routine
+extern Offsets acceleration_offs;
+//Initialise as 'acceleration'
+
+extern uint8_t ACCEL_X_H;
+extern uint8_t ACCEL_X_L;
+extern uint8_t ACCEL_Y_H;
+extern uint8_t ACCEL_Y_L;
+extern uint8_t ACCEL_Z_H;
+extern uint8_t ACCEL_Z_L;
+//Temporary high & low 8-bit storage
 
 /* Function Declarations */
 void config_I2Cmem(uint16_t device, uint16_t memory_add, uint8_t write_mem, uint16_t mem_size, uint16_t data_size);
@@ -35,5 +57,7 @@ void calc_FTa();
 void check_accel();
 
 void startSFT();
+
+void accel_calibration(struct Offsets *acceleration);
 
 #endif // MPU6050_CONF_H
