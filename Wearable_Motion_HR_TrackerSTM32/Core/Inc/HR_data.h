dd189ad4fe5039e5654cad8df06bc4c5b021170f
n04h24 <noahnourdine8@gmail.com>
2026-01-19 06:58:14 +1100

    MAX30102 Modbus protocol __init__

---
+++ Wearable_Motion_HR_TrackerSTM32/Core/Inc/HR_data.h
@@ -0,0 +1,13 @@
+/*
+ * HR_data.h
+ *
+ *  Created on: Jan 19, 2026
+ *      Author: noah
+ */
+
+#ifndef INC_HR_DATA_H_
+#define INC_HR_DATA_H_
+
+
+
+#endif /* INC_HR_DATA_H_ */
---
+++ Wearable_Motion_HR_TrackerSTM32/Core/Src/HR_data.c
@@ -0,0 +1,8 @@
+/*
+ * HR_data.c
+ *
+ *  Created on: Jan 19, 2026
+ *      Author: noah
+ */
+
+
