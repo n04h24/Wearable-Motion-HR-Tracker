3091a23ab358abcfe8f32e435278e027a13134c1
n04h24 <noahnourdine8@gmail.com>
2026-01-26 14:17:45 +1100

    Writing to transmit buffer

--- Wearable_Motion_HR_TrackerSTM32/Core/Inc/HR_data.h
+++ Wearable_Motion_HR_TrackerSTM32/Core/Inc/HR_data.h
@@ -15,6 +15,13 @@
 #define MODBUS_FRAME_WAIT 3.5 /* Char (10-bit) */
 #define RTU_MSG_MAXLENGTH 14
 #define MODBUS_DEV_ADDRESS 0x20
+#define COLLECT_DATA 0x06
+#define COLLECT_DATA_REGADD_A 0x00
+#define COLLECT_DATA_REGADD_B 0x10
+#define COLLECT_REG_NUM_START_A 0x00 /* "Valid Bytes" (Receive) */
+#define COLLECT_REG_NUM_START_B 0x01
+#define COLLECT_REG_NUM_STOP_A 0x00 /* "Data" (Receive) */
+#define COLLECT_REG_NUM_STOP_B 0x02
 
 /* Externs */
 /* typedef struct {
@@ -30,7 +37,7 @@ extern uint8_t MODBUS_WRITE_BUFFER[RTU_MSG_MAXLENGTH];
 extern uint8_t MODBUS_READ_BUFFER[RTU_MSG_MAXLENGTH];
 
 /* Function Declarations */
-static void MODBUS_RTU_format(uint8_t *buffer, uint8_t funct_code);
+static void MODBUS_send_buffer(uint8_t *buffer, uint8_t funct_code, uint8_t reg_addA, uint8_t reg_addB, uint8_t reg_numA, uint8_t reg_numB);
 void MAX30102_collect();
 void MAX30102_HR_SPO2();
 void MAX30102_DIE_TEMP();
--- Wearable_Motion_HR_TrackerSTM32/Core/Src/HR_data.c
+++ Wearable_Motion_HR_TrackerSTM32/Core/Src/HR_data.c
@@ -14,27 +14,21 @@ uint8_t MODBUS_READ_BUFFER[RTU_MSG_MAXLENGTH];
 
 /* Implementations */
 void MAX30102_collect() {
-	/* Collect Data  */
+	/* Format message into buffer */
+	MODBUS_send_buffer(MODBUS_WRITE_BUFFER, COLLECT_DATA, COLLECT_DATA_REGADD_A,
+			COLLECT_DATA_REGADD_B, COLLECT_REG_NUM_START_A, COLLECT_REG_NUM_START_B);
+	/* Send "Collect Data"  */
 	HAL_StatusTypeDef UART_RET = HAL_UART_Transmit(&huart1, MODBUS_WRITE_BUFFER, RTU_MSG_MAXLENGTH, 100);
 	/* Read Response */
 	if (UART_RET == HAL_OK) {
 		HAL_UART_Receive(&huart1, MODBUS_READ_BUFFER, RTU_MSG_MAXLENGTH, 100);
 		/* Match valid bytes (& operation) */
-
-		/* Pass data & length */
-//		CRC_check()
-
-		/* Copy into struct- (readability) */
-
 	}
-
-	/* Stop collecting data */
+	/* Stop collecting data
 	UART_RET = HAL_UART_Transmit(&huart1, MODBUS_WRITE_BUFFER, RTU_MSG_MAXLENGTH, 100);
 	if (UART_RET == HAL_OK) {
 		HAL_UART_Receive(&huart1, MODBUS_READ_BUFFER, RTU_MSG_MAXLENGTH, 100);
-	}
-
-
+	} */
 }
 
 void MAX30102_HR_SPO2() {
@@ -45,18 +39,47 @@ void MAX30102_DIE_TEMP() {
 
 }
 
-static void MODBUS_RTU_format(uint8_t *buffer, uint8_t funct_code) {
-
+static void MODBUS_send_buffer(uint8_t *buffer, uint8_t funct_code, uint8_t reg_addA,
+								uint8_t reg_addB, uint8_t reg_numA, uint8_t reg_numB) {
+	/* Memory helper functions */
+	uint8_t pointer_MSG = 0;
+	uint8_t MACRO_memory_8 = MODBUS_DEV_ADDRESS;
 	/* Device Address (8-bit) */
-	memcpy(buffer, (uint8_t*)MODBUS_DEV_ADDRESS, 1);
-
+	memcpy(buffer, &MACRO_memory_8, 1);
+	pointer_MSG++;
 	/* Function Code (8-bit) */
-
+	MACRO_memory_8 = funct_code;
+	memcpy(&buffer[pointer_MSG], &MACRO_memory_8, 1);
+	pointer_MSG++;
 	/* Register Address or Valid Bytes (16-bit) */
-
+		/* Address (Upper) */
+	MACRO_memory_8 = reg_addA;
+	memcpy(&buffer[pointer_MSG], &MACRO_memory_8, 1);
+	pointer_MSG++;
+		/* Address (Lower) */
+	MACRO_memory_8 = reg_addB;
+	memcpy(&buffer[pointer_MSG], &MACRO_memory_8, 1);
+	pointer_MSG++;
 	/* Register Number or Data (16-bit/N*8-bit) */
+		/* Number (Upper) */
+	MACRO_memory_8 = reg_numA;
+	memcpy(&buffer[pointer_MSG], &MACRO_memory_8, 1);
+	pointer_MSG++;
+		/* Number (Lower) */
+	MACRO_memory_8 = reg_numB;
+	memcpy(&buffer[pointer_MSG], &MACRO_memory_8, 1);
+	pointer_MSG++;
+	/* Calculate CRC */
+	uint16_t crc_val = CRC_check(buffer, pointer_MSG); /* Verify pointer */
+	/* Append CRC (upper) */
+	uint8_t crc_val_A = (uint16_t)crc_val & (0xFF00 >> 8);
+	memcpy(&buffer[pointer_MSG], &crc_val_A, 1);
+	pointer_MSG++;
+	/* Append CRC (lower) */
+	uint8_t crc_val_B = (uint16_t)crc_val & (0x00FF);
+	memcpy(&buffer[pointer_MSG], &crc_val_B, 1);
+	pointer_MSG = 0;
 
-	/* CRC check (16-bit) */
 }
 
 static uint16_t CRC_check(uint8_t *data, uint8_t len) {
