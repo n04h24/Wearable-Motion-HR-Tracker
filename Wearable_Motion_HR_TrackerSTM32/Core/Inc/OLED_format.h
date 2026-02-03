e170f581de5eac01c21d7ae1849031814dfb6a28
n04h24 <noahnourdine8@gmail.com>
2026-02-03 18:26:18 +1100

    Formatted main screen

--- Wearable_Motion_HR_TrackerSTM32/Core/Inc/OLED_format.h
+++ Wearable_Motion_HR_TrackerSTM32/Core/Inc/OLED_format.h
@@ -10,15 +10,19 @@
 
 /* Includes */
 #include "HR_data.h"
+#include "filtering_accel.h"
 #include "ssd1306.h"
 #include "ssd1306_fonts.h"
 
 /* Defines */
 
 /* Extern */
-extern char HR_STRING_BUFF[SAMPLE_HR_COUNT];
+extern char HR_STRING_BUFF[3];
+extern char STEP_STRING_BUFF[5];
+
 /* Function Declarations */
-void HR_DISPLAY();
-void STEP_DISPLAY();
+void START_DISPLAY();
+void display_STEP();
+void display_HR();
 
 #endif /* SRC_OLED_FORMAT_H_ */
--- Wearable_Motion_HR_TrackerSTM32/Core/Src/OLED_format.c
+++ Wearable_Motion_HR_TrackerSTM32/Core/Src/OLED_format.c
@@ -4,26 +4,38 @@
  *  Created on: Jan 16, 2026
  *      Author: noah
  */
+/* Includes */
 #include "OLED_format.h"
 
-
-/* Includes */
-
 /* Variable definitions */
-
+char HR_STRING_BUFF[3];
+char STEP_STRING_BUFF[5];
 
 /* Implementations */
-void HR_DISPLAY() {
-
-	ssd1306_SetCursor(0, 0);
-//	ssd1306_WriteString(test_String, Font_7x10, White);
+void START_DISPLAY() {
+	/* Constant char */
+	char STEP_text[] = "Total Steps: ";
+	char HR_text[] = "HR: ";
+	char BPM_text[] = " bpm";
+	/* Step title */
+	ssd1306_SetCursor(1, 1);
+	ssd1306_WriteString(STEP_text, Font_7x10, White);
+	/* HR titles */
+	ssd1306_SetCursor(5, 3);
+	ssd1306_WriteString(HR_text, Font_7x10, White);
+	ssd1306_SetCursor(12, 3);
+	ssd1306_WriteString(BPM_text, Font_7x10, White);
 	ssd1306_UpdateScreen();
 }
 
+void display_STEP(){
+	sprintf(STEP_STRING_BUFF,"%f", MagSamples[0].Magnitude);
+	ssd1306_SetCursor(14, 1);
+	ssd1306_UpdateScreen();
+}
 
-void STEP_DISPLAY(){
-	ssd1306_SetCursor(0, 0);
-//	ssd1306_WriteString(test_String, Font_7x10, White);
+void display_HR() {
+	ssd1306_SetCursor(9, 3);
 	ssd1306_UpdateScreen();
 }
 
